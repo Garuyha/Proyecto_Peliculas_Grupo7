83b8fbd80a7069c55120eddf6320fa94c7c77df0
Ezequiel Alejandro Garcia <112414829+Garuyha@users.noreply.github.com>
2022-09-22 15:33:11 -0300

    Cambios

--- src/Componentes/MoviesCards.jsx
+++ src/Componentes/MoviesCards.jsx
@@ -1,18 +1,55 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import styles from "../Modulos/MovieCard.module.css";
+import Button from 'react-bootstrap/Button';
+import Overlay from 'react-bootstrap/Overlay';
+import Popover from 'react-bootstrap/Popover';
+import Card from 'react-bootstrap/Card';
 
-export function MoviesCards({movie}){
+export function MoviesCards({ movie }) {
+    const [show, setShow] = useState(false);
+    const [target, setTarget] = useState(null);
+    const ref = useRef(null);
+
+    const handleClick = (event) => {
+        setShow(!show);
+        setTarget(event.target);
+    };
+
     const imageUrl = "https://image.tmdb.org/t/p/w300" + movie.poster_path;
-    return(        
+    return (
         <li className={styles.cards}>
-                <img 
-                    width={230}
-                    height={345}
-                    className={styles.movieImage} 
-                    src ={imageUrl} 
-                    alt = {movie.title}
-                />
-                <div>{movie.title}</div>
-        </li>        
+            <img
+                width={230}
+                height={345}
+                className={styles.movieImage}
+                src={imageUrl}
+                alt={movie.title}
+            />
+            <div>{movie.title}</div>
+
+            <div ref={ref}>
+                <Button onClick={handleClick}>Show synopsis</Button>
+
+                <Overlay
+                    show={show}
+                    target={target}
+                    placement="bottom"
+                    container={ref}
+                    containerPadding={20}
+                >
+                    <Popover id="popover-contained">
+                        <Popover.Body>
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Body>
+                                    <Card.Text>
+                                        {movie.overview}
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Popover.Body>
+                    </Popover>
+                </Overlay>
+            </div>
+        </li>
     );
 }
